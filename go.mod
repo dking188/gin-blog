ae5060403e6f66755ac0c7c279029b04efdc1b48
dking <626600175@qq.com>
2019-06-23 05:25:05 +0800

    Init Commit

---
+++ go.mod
@@ -0,0 +1,8 @@
+module gin-blog
+
+go 1.12
+
+require (
+	github.com/gin-gonic/gin v1.4.0 // indirect
+	github.com/swaggo/gin-swagger v1.1.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,95 @@
+package main
+
+import (
+	_ "./docs"
+	"github.com/swaggo/gin-swagger"
+	"github.com/swaggo/gin-swagger/swaggerFiles"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+var db = make(map[string]string)
+
+func setupRouter() *gin.Engine {
+	// Disable Console Color
+	// gin.DisableConsoleColor()
+	r := gin.Default()
+
+	// Ping test
+	r.GET("/ping", func(c *gin.Context) {
+		c.String(http.StatusOK, "pong")
+	})
+
+	// Get user value
+	r.GET("/user/:name", func(c *gin.Context) {
+		user := c.Params.ByName("name")
+		value, ok := db[user]
+		if ok {
+			c.JSON(http.StatusOK, gin.H{"user": user, "value": value})
+		} else {
+			c.JSON(http.StatusOK, gin.H{"user": user, "status": "no value"})
+		}
+	})
+
+	// Authorized group (uses gin.BasicAuth() middleware)
+	// Same than:
+	// authorized := r.Group("/")
+	// authorized.Use(gin.BasicAuth(gin.Credentials{
+	//	  "foo":  "bar",
+	//	  "manu": "123",
+	//}))
+	authorized := r.Group("/", gin.BasicAuth(gin.Accounts{
+		"foo":  "bar", // user:foo password:bar
+		"manu": "123", // user:manu password:123
+	}))
+
+	authorized.POST("admin", func(c *gin.Context) {
+		user := c.MustGet(gin.AuthUserKey).(string)
+
+		// Parse JSON
+		var json struct {
+			Value string `json:"value" binding:"required"`
+		}
+
+		if c.Bind(&json) == nil {
+			db[user] = json.Value
+			c.JSON(http.StatusOK, gin.H{"status": "ok"})
+		}
+	})
+
+	return r
+}
+
+// @title 测试
+// @version 0.0.1
+// @description  测试
+// @BasePath /api/v1/
+func main() {
+	//r := setupRouter()
+	// Listen and Server in 0.0.0.0:8080
+	//r.Run(":8080")
+	r := gin.New()
+
+	// 创建路由组
+	v1 := r.Group("/api/v1")
+
+	v1.GET("/record/:userId", record)
+
+	// 文档界面访问URL
+	// http://127.0.0.1:8080/swagger/index.html
+	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
+
+	r.Run()
+}
+
+// @获取指定ID记录
+// @Description get record by ID
+// @Accept  json
+// @Produce json
+// @Param   some_id     path    int     true        "userId"
+// @Success 200 {string} string	"ok"
+// @Router /record/{some_id} [get]
+func record(c *gin.Context) {
+	c.String(http.StatusOK, "ok")
+}
